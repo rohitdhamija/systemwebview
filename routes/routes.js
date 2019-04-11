c069ecf3ada126e0b1ef555ffcbf3bb67cce4ad2
rohitdhamija <rohit.dhamija@gmail.com>
2019-04-11 16:14:51 +0530

    updated vbcs proxy code

--- routes/routes.js
+++ routes/routes.js
@@ -7,30 +7,56 @@ app.get("/", function(req, res) {
     
  
 app.post("/webviewparamsvbcs",function(request,response){
-
 console.log(JSON.stringify(request.body));
 
-    result = 'dummy';
+
+    var fname, lname, merchant, amount, date, callbackurl;
  request.body.parameters.forEach(parameter => {
                 if (parameter.key === 'firstname') {
-                    result = parameter.value;
+                    fname = parameter.value;
+                    
+                }
+                if (parameter.key === 'lastname') {
+                    lname = parameter.value;
+                    
+                }
+              if (parameter.key === 'merchant') {
+                    merchant = parameter.value;
+                    
+                }
+              if (parameter.key === 'amount') {
+                    amount = parameter.value;
+                    
+                }
+              if (parameter.key === 'date') {
+                    date = parameter.value;
+                    
+                }     
+                if (parameter.key === 'webview.onDone') {
+                    callbackurl = parameter.value;
                     
                 }
  });
+    
+    console.log("fname: "+fname);
+    console.log("lname: "+lname);
+    console.log("merchant: "+merchant);
+    console.log("date: "+date);
+    console.log("amount: "+amount);    
+    console.log("callbackurl: "+callbackurl);
 
-//    var result = request.body.parameters.firstname;
-   
     
-    console.log("the firstname is: "+result);
+    var webviewURL = 'https://abcs1-oracletemplates.builder.us.oraclecloud.com/rt/testwebview/1.0/webApps/testwebviewapp/?fName='+fname+'&lName='+lname+'&merchant='+merchant+'&date='+date+'&amount='+amount+'&callbackURL='+callbackurl;
     
-    var vbcsurl = 'https://abcs1-oracletemplates.builder.us.oraclecloud.com/rt/testwebview/1.0/webApps/testwebviewapp/?firstname=' + result;
+    console.log('webviewURL: '+ webviewURL);
     
-    console.log(vbcsurl)
   const resbody = {
-        'webview.url': vbcsurl
+        'webview.url': webviewURL
     };
     console.log('Response body: ' + JSON.stringify(resbody));
-    response.json(resbody);
+    response.json(resbody);    
+
+
     
     
 });
